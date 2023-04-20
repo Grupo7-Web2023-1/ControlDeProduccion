d808141c94b2cee61bb5e74d350e5deeeb96bbf9
yeinerd06 <capitan.yeiner@gmail.com>
2023-04-19 21:01:36 -0500

    validar formulario de login

--- js/registro.js
+++ js/registro.js
@@ -5,6 +5,21 @@ function validarFormulario() {
     if (contraseña.length >= 8 || contraseña != confirmarContraseña) {
         // Las contraseñas no coinciden, mostrar mensaje de error
         alert("Las contraseñas no coinciden. Por favor, inténtalo de nuevo.");
+    } else
+        window.open("registrado.html");
+}
+
+
+function logear() {
+    var contraseña = document.getElementById("password").value;
+    var usuario = document.getElementById("username").value;
+
+    if (contraseña.length < 8) {
+        // Las contraseñas no coinciden, mostrar mensaje de error
+        alert("La contraseña es muy corta");
+    } else if (usuario.length < 5) {
+        alert('ingrese un usuario válido')
+    } else {
+        window.open("registrado.html");
     }
-    if (a) window.open("registrado.html");
 }
